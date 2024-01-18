137e5681ca59f627549ad8c1ff1162423a817658
Peter Leonov <gojpeg@gmail.com>
2024-01-18 22:58:51 +0100

    quick test

---
+++ gossip-glomers/echo/test_echo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+{
+  echo '{"src":"p1", "dest": "n1", "body":{"type":"init", "msg_id": 1, "node_id": "n1", "node_ids": []}}'
+  echo '{"src":"n1", "dest": "n2", "body":{"type":"echo", "msg_id": 1, "echo": "foobar"}}'
+ } | cargo run --bin echo
+ 
