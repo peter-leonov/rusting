af18dd4bb25b54433fc9cf703838f47bd3ac64ec
Peter Leonov <gojpeg@gmail.com>
2024-01-21 22:52:22 +0100

    no cpopy

--- src/bin/broadcast_a.rs
+++ src/bin/broadcast_a.rs
@@ -21,11 +21,11 @@ struct Read {
     msg_id: usize,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-struct ReadOK {
+#[derive(Serialize, Debug)]
+struct ReadOK<'a> {
     msg_id: usize,
     in_reply_to: usize,
-    messages: Vec<i32>,
+    messages: &'a Vec<i32>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -42,17 +42,22 @@ struct TopologyOK {
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(tag = "type")]
-enum Body {
+enum BodyIn {
     #[serde(rename = "broadcast")]
     Broadcast(Broadcast),
-    #[serde(rename = "broadcast_ok")]
-    BroadcastOK(BroadcastOK),
     #[serde(rename = "read")]
     Read(Read),
-    #[serde(rename = "read_ok")]
-    ReadOK(ReadOK),
     #[serde(rename = "topology")]
     Topology(Topology),
+}
+
+#[derive(Serialize, Debug)]
+#[serde(tag = "type")]
+enum BodyOut<'a> {
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOK(BroadcastOK),
+    #[serde(rename = "read_ok")]
+    ReadOK(ReadOK<'a>),
     #[serde(rename = "topology_ok")]
     TopologyOK(TopologyOK),
 }
@@ -74,41 +79,36 @@ pub fn main() -> Result<()> {
     for line in lines {
         let line = line.context("reading message")?;
         // dbg!(&line);
-        let message = parse_message::<Body>(&line)?;
+        let message = parse_message::<BodyIn>(&line)?;
 
         match message.body {
-            Body::Broadcast(body) => {
+            BodyIn::Broadcast(body) => {
                 seen.push(body.message);
 
-                let outgoing = Body::BroadcastOK(BroadcastOK {
+                let outgoing = BodyOut::BroadcastOK(BroadcastOK {
                     msg_id: next_message_id(),
                     in_reply_to: body.msg_id,
                 });
 
                 send_message(&mut stdout, &node.id, message.src, outgoing)?;
             }
-            Body::BroadcastOK(_) => {
-                bail!("unexpected broadcast_ok message")
-            }
-            Body::Read(body) => {
-                let outgoing = Body::ReadOK(ReadOK {
+            BodyIn::Read(body) => {
+                let outgoing = BodyOut::ReadOK(ReadOK {
                     msg_id: next_message_id(),
                     in_reply_to: body.msg_id,
-                    messages: seen.clone(),
+                    messages: &seen,
                 });
 
                 send_message(&mut stdout, &node.id, message.src, outgoing)?;
             }
-            Body::ReadOK(_) => bail!("unexpected read_ok message"),
-            Body::Topology(body) => {
-                let outgoing = Body::TopologyOK(TopologyOK {
+            BodyIn::Topology(body) => {
+                let outgoing = BodyOut::TopologyOK(TopologyOK {
                     msg_id: next_message_id(),
                     in_reply_to: body.msg_id,
                 });
 
                 send_message(&mut stdout, &node.id, message.src, outgoing)?;
             }
-            Body::TopologyOK(_) => bail!("unexpected topology_ok message"),
         }
     }
 
