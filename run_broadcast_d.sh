cf7f4fbe246ae9b192e821787428b22c96e29d2c
Peter Leonov <gojpeg@gmail.com>
2024-02-07 20:40:30 +0100

    more nodes

--- run_broadcast_d.sh
+++ run_broadcast_d.sh
@@ -4,4 +4,4 @@ set -e
 
 cargo build --bin broadcast_d
 
-maelstrom test -w broadcast --bin "target/debug/broadcast_d" --node-count 5 --time-limit 20 --rate 10 --nemesis partition
+maelstrom test -w broadcast --bin "target/debug/broadcast_d" --node-count 25 --time-limit 20 --rate 100 --latency 100
