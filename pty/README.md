d4d1172c57da8506ea71c4d9afcc3f700588ecaa
Peter Leonov <gojpeg@gmail.com>
2024-11-30 13:32:43 +0100

    adding net

--- pty/README.md
+++ pty/README.md
@@ -3,3 +3,8 @@
 Trivial telnet server in Rust.
 
 Heavily inspired by Ivan Velichko's [post](https://iximiuz.com/en/posts/linux-pty-what-powers-docker-attach-functionality/) and [source code](https://github.com/iximiuz/ptyme/tree/master).
+
+More useful links:
+
+- [https://stackoverflow.com/questions/74284202/how-to-use-a-pseudo-terminal-returned-from-posix-openpt]
+- [https://github.com/nelhage/reptyr]
--- pty/main.rs
+++ pty/main.rs
@@ -8,21 +8,49 @@ fn start_child() {
 
 fn start_parent(master: std::os::fd::OwnedFd) {
     println!("In the parent process.");
-    std::thread::spawn(move || {
-        let mut stdout = std::io::stdout();
-        let mut file = std::fs::File::from(master);
-        let mut buf = [0u8; 4096];
-        loop {
-            let nread = file.read(&mut buf).unwrap();
-            if nread == 0 {
-                break; // never happens as read() above blocks
-            }
-            stdout.write(&buf[0..nread]).unwrap();
-        }
-    });
 
-    println!("Press Enter to exit parent...");
-    std::io::stdin().read_line(&mut String::new()).unwrap();
+    let master = std::sync::Arc::new(std::fs::File::from(master));
+
+    {
+        let mut master = master.clone();
+        std::thread::spawn(move || {
+            let mut stdout = std::io::stdout();
+            let mut buf = [0u8; 4096];
+            loop {
+                let nread = master.read(&mut buf).unwrap();
+                if nread == 0 {
+                    break; // never happens as read() above blocks
+                }
+                stdout.write(&buf[0..nread]).unwrap();
+            }
+        });
+    }
+
+    let address = "127.0.0.1:12345";
+    let listener = std::net::TcpListener::bind(address).unwrap();
+    println!("Listenning on {address}");
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        dbg!("connected", &stream);
+        stream.write(b"Welcome!\n").unwrap();
+        let mut master = master.clone();
+
+        std::thread::spawn(move || {
+            let mut buf = [0u8; 4096];
+            loop {
+                let nread = master.read(&mut buf).unwrap();
+                if nread == 0 {
+                    break; // never happens as read() above blocks
+                }
+                if let Err(_) = stream.write(&buf[0..nread]) {
+                    break;
+                }
+            }
+
+            dbg!("disconnecting", &stream);
+            drop(stream);
+        });
+    }
 }
 
 fn main() {
